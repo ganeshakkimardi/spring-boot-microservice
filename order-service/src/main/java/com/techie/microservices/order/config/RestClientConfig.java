08c1f1f272b2e909c870b06b476ccd794e60c2d9
Ganesh Akkimardi <ganeshakkimardi2000@gmail.com>
2026-01-15 18:50:34 +0530

    added Circuit Breaker for order service

--- order-service/src/main/java/com/techie/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/techie/microservices/order/config/RestClientConfig.java
@@ -1,27 +1,49 @@
 package com.techie.microservices.order.config;
 
 import com.techie.microservices.order.client.InventoryClient;
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
 
     @Value("${inventory.url}")
     private String inventoryServiceUrl;
+    private final ObservationRegistry observationRegistry;
+
+    public RestClientConfig(ObservationRegistry observationRegistry) {
+        this.observationRegistry = observationRegistry;
+    }
 
     //binding the REST-Client to the HTTP interface we created in inventory client, specific for REST-Client
     @Bean
     public InventoryClient inventoryClient() {
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(3))
+                .build();
+
+        JdkClientHttpRequestFactory requestFactory =
+                new JdkClientHttpRequestFactory(httpClient);
+
+        requestFactory.setReadTimeout(Duration.ofSeconds(3));
+
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(requestFactory)
+                .observationRegistry(observationRegistry)
                 .build();
-        var restClientAdapter = RestClientAdapter.create(restClient);
-        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
-        return httpServiceProxyFactory.createClient(InventoryClient.class);
+
+        var adapter = RestClientAdapter.create(restClient);
+        var factory = HttpServiceProxyFactory.builderFor(adapter).build();
+        return factory.createClient(InventoryClient.class);
     }
 }
